4ad7785f40561f8e084bf9e35dac6d0cc3b41fe6
Nikita Stroganov <findoss@yandex.ru>
2019-03-17 05:04:42 +0300

    move server app

---
+++ server/app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var MyStem = require("./lib/MyStem");
+
+var app = express();
+
+app.use(bodyParser.text());
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.post("/", function(req, res) {
+  var myStem = new MyStem();
+
+  myStem.start();
+
+  // var words = ['карусели', 'немцы', 'печалька'];
+  const words = req.body.split(" ");
+
+  var promises = words.map(function(word) {
+    return myStem.extractAllGrammemes(word);
+  });
+  console.log(words);
+
+  Promise.all(promises).then(function(lemmas) {
+    console.log(lemmas);
+    myStem.stop();
+  });
+
+  res.send("OK");
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
