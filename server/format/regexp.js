473ca337a69226bf3086ea5270a0d43ad4dce04a
Nikita Stroganov <findoss@yandex.ru>
2019-04-23 03:21:13 +0300

    fix regExp

--- server/format/regexp.js
+++ server/format/regexp.js
@@ -1,5 +1,10 @@
 module.exports = PUNCTUATION_MARKS = new RegExp(/[.,\/#!$%\^&\*;:{}=\-_`~()]/, 'g');
 module.exports = LINE_BREAK = new RegExp(/\r/, 'g');
+// FIXME ↓ (incorrect)
+// Идейные
+// информационно-пропагандистское
+// (специалистов)
+// степени
 module.exports = START_WORD = '(^|\\s)(';
 module.exports = END_WORD = ')(?=\\s|$|!|,|.|<)';
 module.exports = WORD = word => new RegExp(START_WORD + word + END_WORD, 'gi');
